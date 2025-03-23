71ea96913afbc2940da55d8bc0b3c92de50ac26b
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-23 16:26:03 +0700

    feat: make a new release

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import App from "./app.js";
 import Carousel from "./carousel.js";
 import Dropdown from "./dropdown.js";
-//import "./dropdown.css";
-//import "./carousel.css";
+import "./dropdown.css";
+import "./carousel.css";
 
 App.register("dropdown", Dropdown);
 App.register("carousel", Carousel);
